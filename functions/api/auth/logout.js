1a29e3c2657cb1bbc9d200b3296363a5c5cef8df
niyaz8951 <152418821+niyaz8951@users.noreply.github.com>
2026-09-16 23:42:23 +0400

    rest

--- functions/api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-import { json, readCookie } from '../../_lib.js';
-
-export const onRequestPost = async ({ env, request }) => {
-  const sid = readCookie(request, 'tn_session');
-  if (sid) await env.DB.prepare('DELETE FROM sessions WHERE id = ?1').bind(sid).run();
-  return json({ ok: true }, 200,
-    { 'Set-Cookie': 'tn_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0' });
-};
